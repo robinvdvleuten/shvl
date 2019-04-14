74b5815e3a14d43b020f844588c176fb5f6d0064
ajenkinski <adam@thejenkins.org>
2019-04-14 10:38:18 -0400

    Add val parameter to set in Typescript file (#6)

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
 export function get (object: object, path: string|string[], def?: any): any;
-export function set (object: object, path: string|string[]): any;
+export function set (object: object, path: string|string[], val: any): any;
